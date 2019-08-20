fe74fa085be4de08bd114260e1156b2ae4fd0100
Дмитрий <acrius@mail.ru>
2019-08-20 23:16:55 +0700

    Update poetry config. Update processing rules structure.

--- html_processor/rules.py
+++ html_processor/rules.py
@@ -5,6 +5,25 @@ class Rule:
     def __init__(self, content):
         self.content = content
 
+    def get_elements(self):
+        area = self.get_area()
+        elements = self.select(area)
+        filtered_elements = self.filter(elements)
+
+        return filtered_elements
+
+    def get_area(self):
+        return self.content
+
+    def select(self, area):
+        return area
+
+    def filter(self, elements):
+        return [element for element in elements if self.select_element(element)]
+
+    def select_element(self, element):
+        return True
+
     def process(self):
         pass
 
@@ -16,18 +35,21 @@ class TagRule(Rule):
         tag = tag or self.tag
         return self.content.new_tag(tag)
 
-    def get_tags(self):
-        return self.content.findAll(self.tag)
+    def select(self, area):
+        return area.findAll(self.tag)
 
     def process(self):
         tags = self.get_tags()
         for tag in tags:
-            attributes = tag.attrs
-            new_tag = self.get_new_tag(attributes)
-            if new_tag:
-                tag.replaceWith(new_tag)
-            elif self.is_extract(attributes):
-                tag.extract()
+            self.process_tag(tag)
+
+    def process_tag(self, tag):
+        attributes = tag.attrs
+        new_tag = self.get_new_tag(attributes)
+        if new_tag:
+            tag.replaceWith(new_tag)
+        elif self.is_extract(attributes):
+            tag.extract()
 
     def get_new_tag(self, attributes):
         tag = self.create_tag()
@@ -40,17 +62,22 @@ class TagRule(Rule):
 
 
 class TextRule(Rule):
+    def select(self, area):
+        return area.findAll(text=True)
+
+    def select_element(self, element):
+        return not isinstance(element, Comment)
+
     def process(self):
-        html_strings = [
-            html_string
-            for html_string in self.content.findAll(text=True)
-            if not isinstance(html_string, Comment) and self.select_string(html_string)
-        ]
+        html_strings = self.get_elements()
         for html_string in html_strings:
-            string = str(html_string)
-            new_string = self.get_new_string(string)
-            if new_string != string:
-                html_string.replace_with(new_string)
+            self.process_string(html_string)
+
+    def process_string(self, html_string):
+        string = str(html_string)
+        new_string = self.get_new_string(string)
+        if new_string != string:
+            html_string.replace_with(new_string)
 
     def get_new_string(self, string):
         return string
